e4e16384560a1d4d3e3e52e00f3e77619770960c
Sebastian Nieto <snrnieto1999@hotmail.com>
2025-02-20 09:11:14 -0500

    [FIX] page initial

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,13 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-
-const LockProviderClient = dynamic(
-  () => import('../context/LockContext').then(mod => mod.LockProvider),
-  { ssr: false }
-);
+import LockInterface from "./components/LockInterface";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <LockProviderClient>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        {/* Your existing page content */}
-      </main>
-    </LockProviderClient>
+    <div className="min-h-screen">
+    <Navbar />
+    <LockInterface />
+  </div>
   );
 }
